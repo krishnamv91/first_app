e762de1c7a9e58bc027ee9be2ec5d582888c8ef7
krishnamv91 <krishnamv91@gmail.com>
2012-11-03 06:50:59 +0530

    vgfh

--- db/migrate/20121101160038_add_student_id_to_lecture.rb
+++
@@ -1,9 +0,0 @@
-class AddStudentIdToLecture < ActiveRecord::Migration
-  def self.up
-    add_column :teachers, :student_id, :integer
-  end
-
-  def self.down
-    remove_column :teachers, :student_id
-  end
-end
